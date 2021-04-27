7816322d733a31ea38f477bf4a58e3f4314c18b2
usuario <usuario@DESKTOP-M9JI9PS>
2021-04-27 10:02:36 +0200

    Agregar archivos de proyecto.

---
+++ PuertaMagica.API/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PuertaMagica.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuertaMagica.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public bool Login(LoginDTO loginDTO) 
+        {
+            if (loginDTO.Username == "Pepe" && loginDTO.Password == "mola")
+                return true;
+            else 
+                return false;
+        }
+    }
+}
